0acabbf121b16a7863b03774202ad07bcf3532e8
Alberto Manzaneque <amanzag@gmail.com>
2024-10-08 15:11:37 +0200

    UX responsiveness.

--- swap-frontend/src/App.tsx
+++ swap-frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import { Component, ParentComponent } from 'solid-js';
-import { Container, Nav, Navbar } from 'solid-bootstrap';
+import { Container } from 'solid-bootstrap';
 import './app.scss';
 import { render } from 'solid-js/web';
 import 'solid-devtools';
@@ -8,34 +8,14 @@ import { SwapOutDetails } from './SwapOutDetails.js';
 import { Route, Router, RouteSectionProps } from '@solidjs/router';
 import { Toaster } from 'solid-toast';
 import { History } from './History.js';
-import logo from '/assets/brand.svg';
 import { SwapForm } from './SwapForm.js';
+import { Header } from './Header.js';
 
 const Layout: Component<RouteSectionProps> = (props) => {
     return <>
         <div id="main">
             <header>
-                <Navbar class="mb-4 pt-4" expand="lg" collapseOnSelect>
-                    <Container>
-                        <Navbar.Brand class="fs-2" href="/">
-                            <img src={logo} style="height: 3rem"/>
-                        </Navbar.Brand>
-                        <Nav class="justify-content-end gap-5">
-                            <Nav.Item>
-                                <Nav.Link link href="/">Swap</Nav.Link>
-                            </Nav.Item>
-                            <Nav.Item>
-                                <Nav.Link link href="/history">History</Nav.Link>
-                            </Nav.Item>
-                            <Nav.Item>
-                                <Nav.Link link href="/faq">FAQ</Nav.Link>
-                            </Nav.Item>
-                            <Nav.Item>
-                                <Nav.Link link href="/contact">Contact</Nav.Link>
-                            </Nav.Item>
-                        </Nav>
-                    </Container>
-                </Navbar>
+                <Header />
             </header>
             {props.children}
         </div>
---
+++ swap-frontend/src/Header.tsx
@@ -0,0 +1,51 @@
+import { Component, createSignal } from 'solid-js';
+import { Container, Nav, Navbar, Offcanvas } from 'solid-bootstrap';
+import logo from '/assets/brand.svg';
+import { useBeforeLeave } from '@solidjs/router';
+
+const NavLinks: Component = () => {
+    return <>
+        <Nav.Item>
+            <Nav.Link link href="/">Swap</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+            <Nav.Link link href="/history">History</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+            <Nav.Link link href="/faq">FAQ</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+            <Nav.Link link href="/contact">Contact</Nav.Link>
+        </Nav.Item>
+    </>;
+};
+
+export const Header: Component = () => {
+    const [showOffCanvas, setShowOffCanvas] = createSignal(false);
+
+    useBeforeLeave(e => setShowOffCanvas(false));
+
+    return <>
+        <Navbar class="mb-4 pt-4" expand="md" collapseOnSelect>
+            <Container>
+                <Navbar.Brand class="fs-2" href="/">
+                    <img src={logo} style="height: 3rem"/>
+                </Navbar.Brand>
+                <Navbar.Toggle class="d-sm-block d-md-none" onClick={() => setShowOffCanvas(true)} />
+                <Nav class="justify-content-end gap-5 d-sm-none d-md-flex">
+                    <NavLinks />
+                </Nav>
+                <Offcanvas show={showOffCanvas()} onHide={setShowOffCanvas(false)}>
+                    <Offcanvas.Header closeButton>
+                        <Offcanvas.Title>40Swap</Offcanvas.Title>
+                    </Offcanvas.Header>
+                    <Offcanvas.Body>
+                        <Nav class="gap-3">
+                            <NavLinks />
+                        </Nav>
+                    </Offcanvas.Body>
+                </Offcanvas>
+            </Container>
+        </Navbar>
+    </>;
+};
