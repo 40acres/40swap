9239ac89ce0a1c15d8f3a2717f78d634bbbafcc3
Alberto Manzaneque <amanzag@gmail.com>
2024-10-08 14:11:18 +0200

    Website UX.

--- swap-frontend/src/App.tsx
+++ swap-frontend/src/App.tsx
@@ -13,17 +13,33 @@ import { SwapForm } from './SwapForm.js';
 
 const Layout: Component<RouteSectionProps> = (props) => {
     return <>
-        <Navbar class="mb-4" expand="lg" collapseOnSelect>
-            <Container>
-                <Navbar.Brand class="fs-2" href="/">
-                    <img src={logo} style="height: 3rem"/>
-                </Navbar.Brand>
-                <Nav class="me-auto">
-                    <Nav.Link link href="/history">History</Nav.Link>
-                </Nav>
-            </Container>
-        </Navbar>
-        {props.children}
+        <div id="main">
+            <header>
+                <Navbar class="mb-4 pt-4" expand="lg" collapseOnSelect>
+                    <Container>
+                        <Navbar.Brand class="fs-2" href="/">
+                            <img src={logo} style="height: 3rem"/>
+                        </Navbar.Brand>
+                        <Nav class="justify-content-end gap-5">
+                            <Nav.Item>
+                                <Nav.Link link href="/">Swap</Nav.Link>
+                            </Nav.Item>
+                            <Nav.Item>
+                                <Nav.Link link href="/history">History</Nav.Link>
+                            </Nav.Item>
+                            <Nav.Item>
+                                <Nav.Link link href="/faq">FAQ</Nav.Link>
+                            </Nav.Item>
+                            <Nav.Item>
+                                <Nav.Link link href="/contact">Contact</Nav.Link>
+                            </Nav.Item>
+                        </Nav>
+                    </Container>
+                </Navbar>
+            </header>
+            {props.children}
+        </div>
+        <footer>TBD</footer>
         <Toaster toastOptions={{
             duration: 5000,
             position: 'bottom-right',
@@ -32,13 +48,13 @@ const Layout: Component<RouteSectionProps> = (props) => {
 };
 
 const NarrowContainer: ParentComponent = (props) => {
-    return <div style="width: 600px; padding: 30px" class="mx-auto" id="main">
+    return <div style="width: 600px" class="mx-auto content">
         {props.children}
     </div>;
 };
 
 const WideContainer: ParentComponent = (props) => {
-    return <Container>
+    return <Container class="content">
         {props.children}
     </Container>;
 };
@@ -49,6 +65,7 @@ const App: Component = () => {
         <Route path="/swap/in/:id" component={() => <NarrowContainer><SwapInDetails /></NarrowContainer>} />
         <Route path="/swap/out/:id" component={() => <NarrowContainer><SwapOutDetails /></NarrowContainer>} />
         <Route path="/history" component={() => <WideContainer><History /></WideContainer>} />
+        <Route path="/*" component={() => <WideContainer><h3 class="text-center">Page not found</h3></WideContainer>} />
     </Router>;
 };
 
--- swap-frontend/src/History.tsx
+++ swap-frontend/src/History.tsx
@@ -1,4 +1,4 @@
-import { Component, createResource, createSignal, For } from 'solid-js';
+import { Component, createResource, createSignal, For, Show } from 'solid-js';
 import { Button, Modal, Table } from 'solid-bootstrap';
 import { applicationContext } from './ApplicationContext.js';
 import { A } from '@solidjs/router';
@@ -34,41 +34,43 @@ export const History: Component = () => {
     }
 
     return <>
-        <h3>Swap history</h3>
-        <Table class="swap-history-table">
-            <thead>
-                <tr>
-                    <th>Type</th>
-                    <th>Id</th>
-                    <th>Date</th>
-                    <th>Actions</th>
-                </tr>
-            </thead>
-            <tbody>
-                <For each={swaps()}>{s => <>
+        <Show when={swaps()?.length ?? 0 > 0} fallback={<h3 class="text-center">No data</h3>}>
+            <h3>Swap history</h3>
+            <Table class="swap-history-table">
+                <thead>
                     <tr>
-                        <td><SwapTypeComponent type={s.type} /></td>
-                        <td><A href={`/swap/${s.type}/${s.swapId}`}>{s.swapId}</A></td>
-                        <td>{new Date(s.createdAt).toLocaleString()}</td>
-                        <td><span onClick={() => setSwapToDelete(s.swapId)}><Fa icon={faTrash} /></span></td>
+                        <th>Type</th>
+                        <th>Id</th>
+                        <th>Date</th>
+                        <th>Actions</th>
                     </tr>
-                </>}</For>
-            </tbody>
-        </Table>
+                </thead>
+                <tbody>
+                    <For each={swaps()}>{s => <>
+                        <tr>
+                            <td><SwapTypeComponent type={s.type} /></td>
+                            <td><A href={`/swap/${s.type}/${s.swapId}`}>{s.swapId}</A></td>
+                            <td>{new Date(s.createdAt).toLocaleString()}</td>
+                            <td><span onClick={() => setSwapToDelete(s.swapId)}><Fa icon={faTrash} /></span></td>
+                        </tr>
+                    </>}</For>
+                </tbody>
+            </Table>
 
-        <Modal show={swapToDelete() != null} centered onHide={() => setSwapToDelete()}>
-            <Modal.Header closeButton>
-                <Modal.Title>Confirmation</Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-                Are you sure you want to delete swap {swapToDelete()}?
-            </Modal.Body>
-            <Modal.Footer>
-                <div class="d-flex flex-grow-1 flex-shrink-0 gap-2">
-                    <Button variant="secondary" onClick={() => setSwapToDelete()}>No</Button>
-                    <Button variant="primary" onClick={() => deleteSwap()}>Yes</Button>
-                </div>
-            </Modal.Footer>
-        </Modal>
+            <Modal show={swapToDelete() != null} centered onHide={() => setSwapToDelete()}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Confirmation</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    Are you sure you want to delete swap {swapToDelete()}?
+                </Modal.Body>
+                <Modal.Footer>
+                    <div class="d-flex flex-grow-1 flex-shrink-0 gap-2">
+                        <Button variant="secondary" onClick={() => setSwapToDelete()}>No</Button>
+                        <Button variant="primary" onClick={() => deleteSwap()}>Yes</Button>
+                    </div>
+                </Modal.Footer>
+            </Modal>
+        </Show>
     </>;
 };
