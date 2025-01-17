91b1e6d71b2d28541bdbca33b0a82d9f82e1a04e
Alberto Manzaneque <amanzag@gmail.com>
2025-01-17 10:14:21 +0100

    fix github icon

--- swap-frontend/src/Footer.tsx
+++ swap-frontend/src/Footer.tsx
@@ -14,9 +14,9 @@ export const Footer: Component = () => <>
                 <div><a href="https://40acres.pro"><img src={fortyAcresBrandUrl} /></a></div>
                 <div><strong>40 ACRES, S.A. DE C.V.</strong><br />Company Registration No. 2024113630 - El Salvador</div>
                 <div class="d-flex gap-3">
-                    <a href="https://x.com/40acres_sv"><img src={twitterImgUrl} title="X" /></a>
-                    <a href="https://www.linkedin.com/company/40acres/"><img src={linkedinImgUrl} title="LinkedIn" /></a>
-                    <a href="https://github.com/40acres/40swap/"><img src={githubImgUrl} title="GitHub" /></a>
+                    <a href="https://x.com/40acres_sv"><img src={twitterImgUrl} height="28" title="X" /></a>
+                    <a href="https://www.linkedin.com/company/40acres/"><img src={linkedinImgUrl} height="28" title="LinkedIn" /></a>
+                    <a href="https://github.com/40acres/40swap/"><img src={githubImgUrl} height="28" title="GitHub" /></a>
                 </div>
                 <div>Copyright © 2024 40Swap.com</div>
             </div>
