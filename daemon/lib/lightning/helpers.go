c5e515632403cbc3b17b0475873f1c482a525a69
Marcos <marferp2@inf.upv.es>
2025-03-14 11:26:08 +0100

    fix: delegate validate preimage

--- daemon/lib/lightning/helpers.go
+++ daemon/lib/lightning/helpers.go
@@ -1,9 +1,7 @@
 package lightning
 
 import (
-	"crypto/sha256"
 	"encoding/hex"
-	"errors"
 
 	"github.com/btcsuite/btcd/btcec/v2"
 )
@@ -21,26 +19,4 @@ func ParsePubKey(pubKeyStr string) (*btcec.PublicKey, error) {
 	}
 
 	return pubKey, nil
-}
-
-// ValidatePreimage validates that a preimage matches a payment hash
-func ValidatePreimage(preimage string, paymentHash string) error {
-	if preimage == "" {
-		return errors.New("preimage is empty")
-	}
-	if paymentHash == "" {
-		return errors.New("payment hash is empty")
-	}
-
-	decodedPreimage, err := hex.DecodeString(preimage)
-	if err != nil {
-		return errors.New("preimage is not a valid hex string")
-	}
-	preimageHash := sha256.Sum256(decodedPreimage)
-	preimageHashString := hex.EncodeToString(preimageHash[:])
-	if paymentHash != preimageHashString {
-		return errors.New("preimage does not match bolt11 payment hash")
-	}
-
-	return nil
 }
--- daemon/lib/lightning/helpers_test.go
+++ daemon/lib/lightning/helpers_test.go
@@ -1,11 +1,9 @@
 package lightning
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/btcsuite/btcd/btcec/v2"
-	"github.com/stretchr/testify/assert"
 )
 
 func TestParsePubKey(t *testing.T) {
@@ -50,68 +48,6 @@ func TestParsePubKey(t *testing.T) {
 
 				return
 			}
-		})
-	}
-}
-
-func TestValidatePreimage(t *testing.T) {
-	validPreimage := "01da5d7d740ac57fcdc15fd47b9c8c0c3ba3b7b762d126df10fab4ad95d98400"
-	validPaymentHash := "d78a8ba8b6251027f37fd6febff0315f2d45be831ba313fb23c6e03a2abe3ca5"
-
-	type args struct {
-		preimage    string
-		paymentHash string
-	}
-	tests := []struct {
-		name string
-		args args
-		want error
-	}{
-		{
-			name: "Invalid preimage (empty)",
-			args: args{
-				preimage:    "",
-				paymentHash: "123",
-			},
-			want: fmt.Errorf("preimage is empty"),
-		},
-		{
-			name: "Invalid payment hash (empty)",
-			args: args{
-				preimage:    "123",
-				paymentHash: "",
-			},
-			want: fmt.Errorf("payment hash is empty"),
-		},
-		{
-			name: "Invalid preimage (not hex)",
-			args: args{
-				preimage:    "not hex",
-				paymentHash: validPaymentHash,
-			},
-			want: fmt.Errorf("preimage is not a valid hex string"),
-		},
-		{
-			name: "Invalid preimage (does not match payment hash)",
-			args: args{
-				preimage:    "5dc1f0d161a089fa7fb1e864f64b1543a592284614c48346c9de61c59c37edc1",
-				paymentHash: validPaymentHash,
-			},
-			want: fmt.Errorf("preimage does not match bolt11 payment hash"),
-		},
-		{
-			name: "Valid preimage",
-			args: args{
-				preimage:    validPreimage,
-				paymentHash: validPaymentHash,
-			},
-			want: nil,
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			err := ValidatePreimage(tt.args.preimage, tt.args.paymentHash)
-			assert.Equal(t, tt.want, err)
 		})
 	}
 }
