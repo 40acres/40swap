3b9b980bf28afb8c0c4c3716766e096a8c1d9c4b
Marcos <marferp2@inf.upv.es>
2025-03-14 11:11:59 +0100

    fix: remove unnecessary code and update mock

--- daemon/lib/lightning/lightning.go
+++ daemon/lib/lightning/lightning.go
@@ -12,8 +12,6 @@ const DefaultCltvExpiry uint64 = 144
 
 var ErrInvoiceCanceled = fmt.Errorf("invoice canceled")
 
-// This will be the pubkey in the case of direct connection, or the dapr alias in the case of dapr
-type PubKey = string
 type Preimage = string
 type NetworkFeeSats = int64
 
@@ -24,7 +22,6 @@ type GraphStatus struct {
 
 //go:generate mockgen -destination=mock.go -package=lightning . Client
 type Client interface {
-	PubKey() PubKey
 	GetGraphStatus(ctx context.Context) (*GraphStatus, error)
 	PayInvoice(ctx context.Context, paymentRequest string) error
 	MonitorPaymentRequest(ctx context.Context, paymentHash string) (Preimage, NetworkFeeSats, error)
