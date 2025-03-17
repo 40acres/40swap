068533958d8aea5f865f0823415533bb2ddc964b
Marcos <marferp2@inf.upv.es>
2025-03-17 12:01:56 +0100

    fix: amount in uint64

--- daemon/money/money.go
+++ daemon/money/money.go
@@ -4,12 +4,13 @@ import (
 	"github.com/shopspring/decimal"
 )
 
-type Money int64
+// Money is a type that represents a monetary amount in satoshis for Bitcoin.
+type Money uint64
 
 func NewFromBtc(amount decimal.Decimal) Money {
 	return Money(amount.Mul(decimal.NewFromInt(1e8)).IntPart())
 }
 
 func (m Money) ToBtc() decimal.Decimal {
-	return decimal.NewFromInt(int64(m)).Div(decimal.NewFromInt(1e8))
+	return decimal.NewFromUint64(uint64(m)).Div(decimal.NewFromInt(1e8))
 }
