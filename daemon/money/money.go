86581693504e409217e58e96383be4c5ce736384
Marcos <marferp2@inf.upv.es>
2025-03-17 12:07:56 +0100

    fix: add no lint since negative amounts checked

--- daemon/money/money.go
+++ daemon/money/money.go
@@ -1,14 +1,22 @@
 package money
 
 import (
+	"errors"
+
 	"github.com/shopspring/decimal"
 )
 
 // Money is a type that represents a monetary amount in satoshis for Bitcoin.
 type Money uint64
 
-func NewFromBtc(amount decimal.Decimal) Money {
-	return Money(amount.Mul(decimal.NewFromInt(1e8)).IntPart())
+// ErrNegativeAmount is returned when trying to create a Money with a negative amount.
+var ErrNegativeAmount = errors.New("amount cannot be negative")
+
+func NewFromBtc(amount decimal.Decimal) (Money, error) {
+	if amount.IsNegative() {
+		return 0, ErrNegativeAmount
+	}
+	return Money(amount.Mul(decimal.NewFromInt(1e8)).IntPart()), nil // nolint
 }
 
 func (m Money) ToBtc() decimal.Decimal {
