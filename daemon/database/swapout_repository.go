50d547790d2d764091c6b2da52081e31bbf3fba4
José A.P <53834183+Jossec101@users.noreply.github.com>
2025-03-28 14:57:43 +0100

    Remove unused repo methods

--- daemon/database/swapout_repository.go
+++ daemon/database/swapout_repository.go
@@ -4,16 +4,8 @@ import "github.com/40acres/40swap/daemon/database/models"
 
 type SwapOutRepository interface {
 	SaveSwapOut(swapOut *models.SwapOut) error
-	GetSwapOut(id string) (*models.SwapOut, error)
 }
 
 func (d *Database) SaveSwapOut(swapOut *models.SwapOut) error {
 	return d.orm.Save(swapOut).Error
-}
-
-func (d *Database) GetSwapOut(id string) (*models.SwapOut, error) {
-	var swapOut models.SwapOut
-	err := d.orm.First(&swapOut, "id = ?", id).Error
-
-	return &swapOut, err
 }
