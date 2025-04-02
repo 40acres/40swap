e0e0d3e9c44f82181f58999ede306b20c93f04ca
Rodrigo <39995243+RodriFS@users.noreply.github.com>
2025-04-02 12:25:55 +0200

    Launch daemon with lndconnect

--- daemon/cmd/main.go
+++ daemon/cmd/main.go
@@ -84,6 +84,10 @@ func main() {
 				Usage: "Keep the database running after the daemon stops for embedded databases",
 				Value: false,
 			},
+			&cli.StringFlag{
+				Name:  "lndconnect",
+				Usage: "LND connect URI",
+			},
 			&grpcPort,
 			&serverUrl,
 			&tlsCert,
@@ -143,12 +147,20 @@ func main() {
 						return fmt.Errorf("❌ Could not connect to swap server: %w", err)
 					}
 
-					lnClient, err := lnd.NewClient(ctx,
-						lnd.WithLndEndpoint(c.String("lnd-host")),
-						lnd.WithMacaroonFilePath(c.String("macaroon")),
-						lnd.WithTLSCertFilePath(c.String("tls-cert")),
+					options := []lnd.Option{
 						lnd.WithNetwork(rpc.ToLightningNetworkType(network)),
-					)
+					}
+					lndConnect := c.String("lndconnect")
+					if lndConnect != "" {
+						options = append(options, lnd.WithLNDConnectURI(lndConnect))
+					} else {
+						options = append(options,
+							lnd.WithLndEndpoint(c.String("lnd-host")),
+							lnd.WithMacaroonFilePath(c.String("macaroon")),
+							lnd.WithTLSCertFilePath(c.String("tls-cert")))
+					}
+
+					lnClient, err := lnd.NewClient(ctx, options...)
 					if err != nil {
 						return fmt.Errorf("❌ Could not connect to LND: %w", err)
 					}
