4763f8bccd25f8dbd05bc6c25f3553ceca194bfc
Alejandro Acho <alejandro@tribes.studio>
2025-04-02 17:25:35 +0200

    update utxos handling and remove unnecessary things

--- server-backend/src/LiquidService.ts
+++ server-backend/src/LiquidService.ts
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { FourtySwapConfiguration } from './configuration.js';
-import { NbxplorerService, NBXplorerUtxo, NBXplorerUtxosResponse } from './NbxplorerService.js';
+import { NbxplorerService } from './NbxplorerService.js';
 import { Injectable, Logger, Inject, OnApplicationBootstrap, Scope } from '@nestjs/common';
 import axios from 'axios';
 
@@ -9,6 +9,23 @@ export class LiquidConfigurationDetails {
     readonly rpcUrl!: string;
     readonly rpcAuth!: {username: string, password: string};
     public xpub!: string;
+}
+
+export interface RPCUtxo {
+    txid: string;
+    vout: number;
+    address: string;
+    label: string;
+    scriptPubKey: string;
+    amount: number;
+    asset: string;
+    amountblinder: string;
+    assetblinder: string;
+    confirmations: number;
+    spendable: boolean;
+    solvable: boolean;
+    desc: string;
+    safe: boolean;
 }
 
 @Injectable({ scope: Scope.DEFAULT })
@@ -41,17 +58,16 @@ export class LiquidService implements OnApplicationBootstrap  {
         this.logger.debug('Starting to initialize LiquidService xpub');
         try {
             await this.nbxplorer.track(this.xpub, 'lbtc');
-            this.logger.debug('LiquidService xpub initialized successfully');
+            this.logger.log('LiquidService xpub initialized successfully');
         } catch (error: unknown) {
             const errorMessage = error instanceof Error ? error.message : 'Unknown error';
             this.logger.error(`Failed to initialize Liquid xpub: ${errorMessage}`);
         }
     }
 
-    async callRPC(method: string, params: any[] = [], walletName: string = ''): Promise<any> {
-        const url = walletName ? `${this.rpcUrl}/wallet/${walletName}` : this.rpcUrl;
+    async callRPC(method: string, params: unknown[] = []): Promise<unknown> {
         try {
-            const response = await axios.post(url, {
+            const response = await axios.post(this.rpcUrl, {
                 jsonrpc: '1.0',
                 id: '40swap',
                 method,
@@ -61,41 +77,29 @@ export class LiquidService implements OnApplicationBootstrap  {
             });
             return response.data.result;
         } catch (error) {
-            this.logger.error(`Error calling Elements RPC ${method}: ${(error as any).message}`);
+            this.logger.error(`Error calling Elements RPC ${method}: ${(error as Error).message}`);
             throw error;
         }
     }
 
-    async getUtxos(): Promise<NBXplorerUtxosResponse | void> {
-        if (!this.xpub) {
-            this.logger.error('Attempting to get UTXOs with empty xpub');
-            throw new Error('Xpub is not initialized');
-        }
-        const utxoResponse = await this.nbxplorer.getUTXOs(this.xpub, 'lbtc');
-        if (!utxoResponse) {
-            throw new Error('No UTXOs returned from NBXplorer');
-        }
-        return utxoResponse;
-    }
-
-    async getConfirmedUtxos(): Promise<NBXplorerUtxo[]> {
-        const utxoResponse = await this.getUtxos();
-        return utxoResponse?.confirmed.utxOs ?? [];
+    async getUnspentUtxos(): Promise<RPCUtxo[]> {
+        const utxoResponse = await this.callRPC('listunspent');
+        return utxoResponse as RPCUtxo[];
     }
 
     async getConfirmedUtxosAndInputValueForAmount(amount: number): Promise<{ 
-        utxos: NBXplorerUtxo[], 
+        utxos: RPCUtxo[], 
         totalInputValue: number,
     }> {
         let totalInputValue = 0;
-        const confirmedUtxos = await this.getConfirmedUtxos();
+        const confirmedUtxos = await this.getUnspentUtxos();
         if (confirmedUtxos.length === 0) {
             throw new Error('No confirmed UTXOs found');
         }
         const selectedUtxos = [];
         for (const utxo of confirmedUtxos) {
             selectedUtxos.push(utxo);
-            totalInputValue += Number(utxo.value);
+            totalInputValue += Number(utxo.amount) * 1e8;
             if (totalInputValue >= amount) {
                 break;
             }
@@ -104,9 +108,5 @@ export class LiquidService implements OnApplicationBootstrap  {
             throw new Error(`Insufficient funds, required ${amount} but only ${totalInputValue} available`);
         }
         return { utxos: selectedUtxos, totalInputValue };
-    }
-  
-    async signPsbt(psbtBase64: string): Promise<string> {
-        return this.callRPC('walletprocesspsbt', [psbtBase64]);
     }
 }
