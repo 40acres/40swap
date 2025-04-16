023f1c89597c4b4489cc1f5582bb160151578ad7
Alejandro Acho <alejandro@tribes.studio>
2025-04-16 17:17:07 +0200

    restore unnecessary change

--- shared/src/liquid-utils.ts
+++ shared/src/liquid-utils.ts
@@ -1,6 +1,7 @@
 import { bitcoin as bitcoinMainnet, regtest as bitcoinRegtest, testnet as bitcoinTestnet, Network as bitcoinNetwork } from 'bitcoinjs-lib/src/networks.js';
 import { liquid as liquidMainnet, regtest as liquidRegtest, testnet as liquidTestnet, Network as liquidNetwork } from 'liquidjs-lib/src/networks.js';
 
+
 export function getLiquidNetworkFromBitcoinNetwork(network: bitcoinNetwork): liquidNetwork {
     switch (network) {
     case bitcoinMainnet:
