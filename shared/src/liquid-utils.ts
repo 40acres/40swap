a87d16bad099b7d491c4d3fedb87bddc5c993436
Alejandro Acho <alejandro@tribes.studio>
2025-05-15 12:56:10 +0200

    add needed shared function to blind psets

--- shared/src/liquid-utils.ts
+++ shared/src/liquid-utils.ts
@@ -1,6 +1,7 @@
 import { bitcoin as bitcoinMainnet, regtest as bitcoinRegtest, testnet as bitcoinTestnet, Network as bitcoinNetwork } from 'bitcoinjs-lib/src/networks.js';
 import { liquid as liquidMainnet, regtest as liquidRegtest, testnet as liquidTestnet, Network as liquidNetwork } from 'liquidjs-lib/src/networks.js';
 import { ECPairFactory, ECPairInterface } from 'ecpair';
+import secp256k1Module from '@vulpemventures/secp256k1-zkp';
 import * as ecc from 'tiny-secp256k1';
 import * as liquid from 'liquidjs-lib';
 
@@ -44,4 +45,32 @@ export function signLiquidPset(pset: liquid.Pset, preImage: string, key: ECPairI
     finalizer.finalizeInput(inputIndex, () => {
         return {finalScriptWitness: liquid.witnessStackToScriptWitness(stack)};
     });
+}
+
+/**
+ * Blinds the given PSET using the input UTXO data and output blinding pubkeys.
+ * Only blinds outputs that are not fee outputs.
+ */
+export async function blindPset(pset: liquid.Pset, utxos: {
+    blindingPrivateKey: Buffer;
+}[]): Promise<void> {
+    const secp = await (secp256k1Module as unknown as {default: () => Promise<liquid.Secp256k1Interface>}).default();
+    const zkpGenerator = new liquid.ZKPGenerator(
+        secp,
+        liquid.ZKPGenerator.WithBlindingKeysOfInputs(utxos.map((utxo) => utxo.blindingPrivateKey!))
+    );
+    const zkpValidator = new liquid.ZKPValidator(secp as liquid.Secp256k1Interface);
+    const outputBlindingArgs = zkpGenerator.blindOutputs(
+        pset,
+        liquid.Pset.ECCKeysGenerator(secp.ecc),
+    );
+
+    const blinder = new liquid.Blinder(
+        pset,
+        zkpGenerator.unblindInputs(pset),
+        zkpValidator,
+        zkpGenerator,
+    );
+
+    blinder.blindLast({ outputBlindingArgs });
 }
