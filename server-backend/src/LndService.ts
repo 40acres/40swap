def178736c648aed4dedf2df2524ae85d483e9b4
Alberto Manzaneque <amanzag@gmail.com>
2025-06-11 12:49:27 +0200

    feat: basic prometheus config and metric for channel peer aliases.

--- server-backend/src/LndService.ts
+++ server-backend/src/LndService.ts
@@ -2,6 +2,7 @@ import { Inject, Injectable, Logger } from '@nestjs/common';
 import { LightningClient } from './lnd/lnrpc/Lightning.js';
 import { InvoicesClient } from './lnd/invoicesrpc/Invoices.js';
 import { Invoice__Output } from './lnd/lnrpc/Invoice.js';
+import { Channel__Output } from './lnd/lnrpc/Channel.js';
 
 @Injectable()
 export class LndService {
@@ -123,6 +124,18 @@ export class LndService {
                     reject(err);
                 } else {
                     resolve(value!.txid);
+                }
+            });
+        });
+    }
+
+    async getChannelInfo(): Promise<Channel__Output[]> {
+        return new Promise((resolve, reject) => {
+            this.lightning.listChannels({ peerAliasLookup: true }, (err, value) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(value!.channels);
                 }
             });
         });
