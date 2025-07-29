459d7764fa49f0e6950f8f4c050c43e36d6e3d50
Alberto Manzaneque <amanzag@gmail.com>
2025-07-29 19:43:26 +0200

    fix: elements metric

--- server-backend/src/metrics/ElementsMetricProvider.ts
+++ server-backend/src/metrics/ElementsMetricProvider.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { Gauge } from 'prom-client';
 import { PrometheusService } from './PrometheusService.js';
 import { LiquidService } from '../LiquidService.js';
@@ -6,6 +6,7 @@ import { LiquidService } from '../LiquidService.js';
 @Injectable()
 export class ElementsMetricProvider {
     constructor(metrics: PrometheusService, elements: LiquidService) {
+        const logger = new Logger(ElementsMetricProvider.name);
         if (elements.isLiquidEnabled) {
             metrics.registry.registerMetric(
                 new Gauge({
@@ -14,12 +15,17 @@ export class ElementsMetricProvider {
                     labelNames: ['asset', 'wallet'],
                     async collect() {
                         const listWalletsResponse = (await elements.callRPC('listwallets')) as string[];
-                        for (const wallet of listWalletsResponse) {
-                            const getBalanceResponse = (await elements.callRPC('getbalance', [], wallet)) as object;
-                            for (const [asset, balance] of Object.entries(getBalanceResponse)) {
-                                if (typeof balance === 'number') {
-                                    this.labels({ wallet, asset }).set(balance);
+                        for (const walletName of listWalletsResponse) {
+                            try {
+                                const getBalanceResponse = (await elements.callRPC('getbalance', [], walletName)) as object;
+                                for (const [asset, balance] of Object.entries(getBalanceResponse)) {
+                                    if (typeof balance === 'number') {
+                                        const wallet = walletName === '' ? '<empty>' : walletName;
+                                        this.labels({ wallet, asset }).set(balance);
+                                    }
                                 }
+                            } catch (e) {
+                                logger.warn(`Error trying to get liquid balance from wallet "${walletName}": ${(e as Error).message}`);
                             }
                         }
                     },
