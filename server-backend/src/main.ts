ea04daa5994735228c08ace8cbd30f45eae6ad56
Alejandro Acho <alejandro@clovrlabs.com>
2025-08-22 16:24:36 +0200

    add custom log level

--- server-backend/src/main.ts
+++ server-backend/src/main.ts
@@ -4,14 +4,19 @@ import { ConfigService } from '@nestjs/config';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { FortySwapConfiguration } from './configuration.js';
 import { LogLevel } from '@nestjs/common';
+import configurationLoader from './configuration.js';
+import { Logger } from '@nestjs/common';
+
+const logger = new Logger('ApplicationBootstrap');
 
 async function bootstrap(): Promise<void> {
+    const config = configurationLoader();
     const app = await NestFactory.create(AppModule, {
-        logger: getLogLevels(),
+        logger: getLogLevels(config.server.environment),
     });
     app.enableShutdownHooks();
-    const config = app.get(ConfigService<FortySwapConfiguration>);
-    const port = config.getOrThrow('server.port', { infer: true });
+    const nestConfig = app.get(ConfigService<FortySwapConfiguration>);
+    const port = nestConfig.getOrThrow('server.port', { infer: true });
     app.setGlobalPrefix('api', { exclude: ['metrics'] });
     const swaggerConfig = new DocumentBuilder().setTitle('40Swap').setDescription('The 40Swap REST API').setVersion('1.0').build();
     const document = SwaggerModule.createDocument(app, swaggerConfig);
@@ -19,10 +24,12 @@ async function bootstrap(): Promise<void> {
     await app.listen(port);
 }
 
-function getLogLevels(): LogLevel[] {
-    if (process.env.NODE_ENV === 'development') {
+function getLogLevels(environment?: string): LogLevel[] {
+    if (environment === 'development' || environment === 'dev' || process.env.NODE_ENV === 'development') {
+        logger.log('Log level set to development');
         return ['log', 'error', 'warn', 'debug', 'verbose', 'fatal'];
     }
+    logger.log('Log level set to production');
     return ['log', 'error', 'warn', 'fatal'];
 }
 
